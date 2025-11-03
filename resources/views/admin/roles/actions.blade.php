3076fff6573f896b8fd4924b4b0f579b4aa1cc48
LinkB-FN <luism.berdugo@outlook.com>
2025-11-03 15:32:12 -0600

    small fixes

---
+++ resources/views/admin/roles/actions.blade.php
@@ -0,0 +1,8 @@
+<div class="felx items-center space x-2">
+    <x-wire-button href="{{ route('admin.roles.edit', $role) }}" blue xs>
+        <i class="fa-solid fa-pen-to-square"></i>
+
+    </x-wire-button>
+
+
+</div>
